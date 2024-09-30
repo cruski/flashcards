05572b9723f03acb363313844b7cc952e64791a6
cruski <159458324+cruski@users.noreply.github.com>
2024-09-30 12:25:50 -0300

    Add files via upload

---
+++ flashcards-main/perguntas.js
@@ -0,0 +1,17 @@
+criaCARTAO(
+    'Programação',
+   'O que é JavaScript?',
+    'O JavaScript é uma linguagem de progração'
+)
+
+criaCARTAO(
+    'Geografia',
+    'Qual a caoital da França?',
+    'A capital da França é Paris'
+)
+
+criaCARTAO(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
