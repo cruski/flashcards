68f7d82050a3d1c9b66bfe0e91d9acbd0e191645
cruski <159458324+cruski@users.noreply.github.com>
2024-10-03 12:25:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCARTAO(
+    'Programação',
+   'O que é Python?',
+    'O Python é uma linguagem de progração'
+)
+
+criaCARTAO(
+    'Geografia',
+    'Qual a caoital da França?',
+    'A capital da França é Paris'
+)
+
+criaCARTAO(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
